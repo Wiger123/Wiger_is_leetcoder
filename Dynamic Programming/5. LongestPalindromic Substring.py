87ac741ef1ad8c8b9f9374ee8c4010258f24edd6
Wiger <31722033+Wiger123@users.noreply.github.com>
2020-12-23 22:00:25 +0800

    Update 5. LongestPalindromic Substring.py

--- Dynamic Programming/5. LongestPalindromic Substring.py
+++ Dynamic Programming/5. LongestPalindromic Substring.py
@@ -23,23 +23,38 @@ class Solution(object):
         dp = [[False] * n for i in range(n)]
 
         # 初始化结果
-        ans = ""
-
-        # 外层嵌套遍历 P(i, 0:n)
-        for i in range(n):
-            # 内层嵌套遍历 P(i + 1:n, j)
-            for j in range(i, n):
+        max_ans = ""
+        
+        # 初始化最大长度, 随时记录用于减少初始化结果替换次数
+        max_len = 0
+        
+        # 计算顺序应该是: (0,0) -> (1,1) -> (2,2) -> (3,3) -> (n-1,n-1) -> (0,1) -> (1,2) -> (2,3) -> (n-2,n-1) -> ... -> (0,n-2) -> (1,n-1) -> (0,n-1)
+        # 如果用图像显示出来, 大致是通过来回的折线访问了半个三角形
+        # 外层循环: 结束位置减去起始位置从 0 到 n - 1, 长度等于该数值 + 1
+        for l in range(n):
+            # 内层循环: 起始横坐标从 0 到 n - l - 1
+            for i in range(n - l - 1):
                 # 判断是否满足边界条件 1
-                if j == i:
-                    dp[i][j] = True
-
+                if l == 0:
+                    dp[i][i + l] = True
+                    # 跳过本次循环
+                    continue
+                    
                 # 判断是否满足边界条件 2
-                elif j == i + 1:
+                if l == 1:
                     # 判断相邻两个字符是否相同
-                    if s[j] == s[i]:
-                        dp[i][j] = True
+                    if s[i] == s[i + l]:
+                        dp[i][i + l] = True
+                    # 跳过本次循环
+                    continue
 
                 # 非边界条件, 转移方程生效
-                else:
-                    dp[i][j] = dp[i + 1][j - 1] and s[i] == s[j]
-
+                dp[i][i + l] = dp[i + 1][i + l - 1] and s[i] == s[i + l - 1]
+                
+                # 判断当前长度是否替换最大长度
+                if dp[i][i + l] and l > max_len:
+                    max_ans = s[i:i + l + 1]
+                    max_len = l
+        
+        # 返回最终结果
+        return max_ans
